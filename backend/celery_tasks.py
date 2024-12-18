7372ff12f9d8208df92c515b03fc0e0647855419
Ilya Perov <petrovilya2013@gmail.com>
2024-12-18 21:14:59 +0300

    First commit

---
+++ backend/celery_tasks.py
@@ -0,0 +1,38 @@
+# tasks.py
+from celery import Celery
+from fastapi_mail import FastMail, MessageSchema
+from email_config import conf
+import asyncio
+import logging
+
+# Настройка логирования
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+)
+logger = logging.getLogger("celery")
+
+celery = Celery("tasks", broker="redis://redis:6379/0")
+
+import logging
+
+logger = logging.getLogger("celery")
+
+@celery.task
+def send_confirmation_email(email: str, token: str):
+    async def send_email():
+        try:
+            message = MessageSchema(
+                subject="Email Confirmation",
+                recipients=[email],
+                body=f"Please confirm your email by clicking the following link: http://localhost:8000/api/auth/confirm/{token}",
+                subtype="html"
+            )
+            fm = FastMail(conf)
+            await fm.send_message(message)
+            logger.info(f"Email sent to {email}")
+        except Exception as e:
+            logger.error(f"Failed to send email to {email}: {e}")
+            raise
+
+    asyncio.run(send_email())
